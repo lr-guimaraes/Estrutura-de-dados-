dcb892b0bca13df0aab105db8c25b5a237b10afd
Leandro Guimarães <leandrorg307@gmail.com>
2022-03-09 15:11:42 -0300

    update

--- pilha_Python.py
+++ pilha_Python.py
@@ -1,39 +1,39 @@
 class Stack:
     def __init__(self):
-        self.stack = [], self.cont = 0
+        self.__stack = [], self.cont = 0
         self.capacity = 1000
-
-    def push(self):
-        str = input("Item: ")
+    
+    def push(self, str):
         self.cont += 1
-        self.stack.append(str,0)
+        self.__stack.append(str,0)
 
     def pop(self):
-        str = input("Item: ")
-        self.stack.append(str)
+        self.__stack.append(str)
 
-    def empty():
-        if self.stack is None:
+    def empty(self):
+        if self.__stack is None:
             print("A pilha está vazia")
         else:
             print ("A pilha não está vazia")
 
-    def full():
-        if 0 + 1 < 1000:
+    def full(self):#arrumar 
+        if len(self.__stack) < 1000:
             print("A lista não está cheia")
         else:
-            print("A pilha esta vazia")
+            print("A pilha esta cheia")
             
-    #def lenght():
-    #    return print(len(self.stack))
+    def lenght(self):
+        return len(self.__stack)
 
-  # def capacit(self):
-   #     print(self.capacity)
+    def capacit(self):
+        print(self.capacity)
 
 
 opcao = input("1 - Adicionar item no inicio \n2 - Adicionar item no final \n3 - Verificar se a pilha possui strings \n4 - Verificar se a pilha está cheia \n5 - Tamanho da pilha \n6 - Capacidade da pilha \n Opção:")
 if opcao == 1:
-    Stack.push()
+    for i in range(1, 1001):
+        Stack.push(i)
+        print(Stack.lenght())
 elif opcao == 2:
     Stack.pop()
 elif opcao == 3:
