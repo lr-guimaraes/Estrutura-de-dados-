04fd23c59b59eac704a694b2fe5f72a2c7295cc0
Leandro Guimarães <leandrorg307@gmail.com>
2022-03-09 16:13:07 -0300

    update 2

--- pilha_C.c
+++ pilha_C.c
@@ -1,22 +1,78 @@
 include <stdio.h>
 
-int main{
-        int option;
-        printf("1 - Adicionar item no inicio \n2 - Adicionar item no final \n3 - Verificar se a pilha possui strings \n4 - Verificar se a pilha está cheia \n5 - Tamanho da pilha \n6 - Capacidade da pilha");
-        print("Option: ");
-        scanf("%d", &option);
+typedef struct {
+    char *st = [1000]
 
-        switch(option){
+
+}Stack;
+
+push();
+pop();
+empty();
+full();
+lenght();
+capacity();
+
+int main{
+    
+    int option;
+    printf("1 - Adicionar item no inicio \n2 - Adicionar item no final \n3 - Verificar se a pilha possui strings \n4 - Verificar se a pilha está cheia \n5 - Tamanho da pilha \n6 - Capacidade da pilha");
+    print("Option: ");
+    scanf("%d", &option);
+    switch(option){
         case 1:
-            strings 
-        /*    case 2:
+            push();
+        case 2:
+            pop();
         case 3: 
+            empty();
         case 4: 
+            full();
         case 5:   
-        case 6: */
+            lenght();
+        case 6: 
+            capacity();
+
         default:
             printf("this option does not exist");       
+        
         }
         return 0;
-} 
-def 
+}
+def push (){ //colocar
+    for (i = 0; i<1000; i++){
+        Stack.st[i] = i;        
+    }
+}
+def pop(){ //ler
+    char *p
+    for (i = 0; i < 1000; i++){
+        p = &Stack.st[i];
+        print(p);
+    }
+    
+}
+def empty(){ 
+    if Stack.st == null{
+        printf("A lista esta vazia");
+    }else{
+        printf("A lista nao esta vazia");
+    }
+
+}
+def full(){
+    if Stack.st == 999{
+        printf("A lista esta cheia");
+    }else{
+        printf("A lista esta cheia");
+    }
+
+}
+def int lenght(){
+    printf(sizeof(Stack.st);
+
+}
+def capacity(){
+    printf("Poderam ser armazenados mais: ", 1000 - sizeof(Stack.st), " itens");
+
+}
--- pilha_Python.py
+++ pilha_Python.py
@@ -1,14 +1,14 @@
 class Stack:
     def __init__(self):
-        self.__stack = [], self.cont = 0
+        self.__stack = [] 
+        self.cont = 0
         self.capacity = 1000
     
     def push(self, str):
         self.cont += 1
-        self.__stack.append(str,0)
-
-    def pop(self):
         self.__stack.append(str)
+    def pop(self):
+        self.__stack.pop()
 
     def empty(self):
         if self.__stack is None:
@@ -29,27 +29,32 @@ class Stack:
         print(self.capacity)
 
 
-opcao = input("1 - Adicionar item no inicio \n2 - Adicionar item no final \n3 - Verificar se a pilha possui strings \n4 - Verificar se a pilha está cheia \n5 - Tamanho da pilha \n6 - Capacidade da pilha \n Opção:")
-if opcao == 1:
-    for i in range(1, 1001):
-        Stack.push(i)
-        print(Stack.lenght())
-elif opcao == 2:
-    Stack.pop()
-elif opcao == 3:
-    Stack.empty()
-elif opcao == 4:
-    Stack.full()
-elif opcao == 5:
-    Stack.lenght()
-elif opcao == 6:
-    Stack.capacit()
+    opcao = input("1 - Adicionar item no inicio \n2 - Adicionar item no final \n3 - Verificar se a pilha possui strings \n4 - Verificar se a pilha está cheia \n5 - Tamanho da pilha \n6 - Capacidade da pilha \n Opção:")
+ while True:
+    pilha = Stack()
+    if opcao == 1:
+        for i in range(1, 1001):
+            pilha.push(i)
+            print(pilha.lenght())
+        pilha.pop()
+    elif opcao == 2:
+        pilha.pop()
+        print(pilha.lenght())
+    elif opcao == 3:
+        pilha.empty()
+    elif opcao == 4:
+        pilha.full()
+    elif opcao == 5:
+        pilha.lenght()
+    elif opcao == 6:
+        pilha.capacit()
 
-'''
-from operator import length_hint
-from os import popen
-from numpy import append
-from pytz import InvalidTimeError
-from tkinter import S
+    question = (input("Deseja continuar? {S/N}: "))
+    if (question.upper() == "N"):
+        False
+    elif (question.upper() != "S" or != "N"):
+        print("Resposta inexistente")
 
-'''
+        
+    
+        
